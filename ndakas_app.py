d1b18776922bc87c8a78cd678c8b5f2013429656
dgichiri <dgichiri@gmail.com>
2024-06-06 19:16:43 +0300

    Update and rename streamlit_app.py to ndakas_app.py

---
+++ ndakas_app.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import streamlit as st
+import pandas as pd
+import random
+
+st.write("""
+ 
+# Welcome to SAFARICOM ENCHANTED AFRICAN GALA
+        
+         """)
+
+msgs = ['Karibu Ndakas, leo unakaa best. Tuishie pale mbele tutulie kama mabingwa',
+        'Karibu sana Shine Kenya ni Tifi sana kukucheki, endelea kupiga hustle yako safi, sisi wote tukue mabingwa!',
+        'Nacheki luku ni diriba, hustle pia imekubali. Muokalize tushine ka mabingwa' ]
+
+
+f_name = st.text_input('Andika Jina')
+
+    
+    
+if st.button('SUBMIT'):
+    intpos = random.randint(0,len(msgs))
+    name = f_name.capitalize()
+    msg = msgs[intpos]
+    
+    st.write(name + " " + msg)
